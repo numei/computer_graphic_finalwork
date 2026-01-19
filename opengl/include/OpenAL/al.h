528d337b7a62beeafe3d1f9ef2401191d4fbba87
Li Junpeng s338989 <1807024877@qq.com>
2026-01-20 00:14:26 +0100

    feat: add OpenAL wrapper headers for cross-platform compatibility (Mac/Windows)

---
+++ opengl/include/OpenAL/al.h
@@ -0,0 +1,4 @@
+#pragma once
+// Wrapper header to redirect OpenAL/al.h to AL/al.h (vcpkg path)
+#include <AL/al.h>
+
---
+++ opengl/include/OpenAL/alc.h
@@ -0,0 +1,4 @@
+#pragma once
+// Wrapper header to redirect OpenAL/alc.h to AL/alc.h (vcpkg path)
+#include <AL/alc.h>
+
